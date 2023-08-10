923064cf90878b4534247ac05129de34094bfcf2
Dhiraj Patra <dhiraj.patra@gmail.com>
2023-08-10 08:55:45 +0530

    updated

--- copy.rs
+++ copy.rs
@@ -4,6 +4,8 @@ struct Point(i32, i32);
 fn main() {
 	let p1 = Point(3, 4);
 	let p2 = p1;
+	// After the assignment, both p1 and p2 own their own data.
+	// We can also use p1.clone() to explicitly copy the data
 	println!("p1: {p1:?}");
 	println!("p2: {p2:?}");
 
